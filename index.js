d8860913b3cd11fa85082ba821825b26884e78f8
Hthehacker <haidaransari800206@gmail.com>
2026-01-04 14:20:14 +0530

    Use environment variable for server port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const router = require("./routes/router")
 const path = require('path');
 const connectdb = require("./config/database")
+const dotenv = require("dotenv")
 connectdb()
 
 app.set("view engine",'ejs')
@@ -12,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router)
 
-app.listen("2000",()=>{
-    console.log("App is listening on port 2000");
+app.listen(process.env.PORT,()=>{
+    console.log(`App is listening on port ${process.env.PORT}`);
     
 })
